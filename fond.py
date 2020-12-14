136431c00bae4223015b46a07ad3596f9f74d4bd
elisa <elisabeaucamp@yahoo.fr>
2020-12-14 17:18:43 +0100

    fond et vaisseau

---
+++ fond.py
@@ -0,0 +1,80 @@
+'''auteur : Elisa
+Date : 14 décembre
+To do : fond du jeu'''
+
+from tkinter import Tk, Canvas, Button, PhotoImage, Frame, Menu
+
+
+class vaisseau :
+
+    def __init__(self,line, column):
+        global height
+        self.MaFenetre_pos_x = width - (width - 10)
+        self.MaFenetre_pos_y = height - 10
+        self.rect_vaisseau = Canevas.create_rectangle(self.MaFenetre_pos_x, self.MaFenetre_pos_y, self.MaFenetre_pos_x + 90, self.MaFenetre_pos_y - 20, fill = 'red')
+
+    def deplacer (self, event):
+        touche = event.keysym
+        dX = 20
+        print(touche)
+        if touche == 'z':
+            self.MaFenetre_pos_y -= dX
+        if touche == 's':
+            self.MaFenetre_pos_y += dX
+        if touche == 'd':
+            self.MaFenetre_pos_x -= dX
+        if touche == 'q':
+            self.MaFenetre_pos_x += dX
+
+        Canevas.coords(self.rect_vaisseau, self.MaFenetre_pos_x - (dX/2), self.MaFenetre_pos_y - (dX/2),  self.MaFenetre_pos_x + (dX/2), self.MaFenetre_pos_y + (dX/2))
+
+#    def projectile (self) :
+        
+
+
+
+
+#Création de la fenêtre principale
+MaFenetre = Tk()
+MaFenetre.title("Space Invaders")
+width = 800
+height = 500
+dim = 20
+
+#Image de fond
+#photo = PhotoImage(file = 'Images/Terre.gif')
+
+# creation d'un widget Frame dans la fenetre principale
+Frame1 = Frame(MaFenetre,relief='groove', bg = 'yellow')
+Frame1.pack(side='left',padx=10,pady=10)
+
+# creation d'un widget Frame dans la fenetre principale
+Frame2 = Frame(MaFenetre,relief='groove')
+Frame2.pack(side='left',padx=10,pady=10)
+
+#Création d'un widget canvas
+Canevas = Canvas(Frame1, height = height, width = width, bg = 'black')
+#tem = Canevas.create_image(0,0, anchor = 'sw', image = photo)
+#print('Image de fond(item', item,")")
+Canevas.focus_set()
+#Canevas.bind('<Key>', vaisseau.deplacer(event))
+Canevas.pack()
+
+#Création d'un bouton Quitter
+BoutonQuitter = Button(Frame2, text = 'Quitter', command = MaFenetre.destroy)
+BoutonQuitter.pack(anchor = 'nw', padx = 10, pady = 10)
+
+#Création d'un bouton Start
+BoutonStart = Button(Frame2, text = 'Start')
+BoutonStart.pack(anchor = 'sw', padx = 10, pady = 10)
+
+#Création d'un menu
+mainmenu = Menu(Frame1)
+menu = Menu(mainmenu,tearoff = 0)
+mainmenu.add_cascade(label = 'Menu', menu = menu)
+menu.add_command(label='A propos')
+menu.add_command(label='Quitter', command = menu.destroy)
+
+vaisseau = vaisseau(line = height, column = 1)
+MaFenetre.config(menu = mainmenu)
+MaFenetre.mainloop()
