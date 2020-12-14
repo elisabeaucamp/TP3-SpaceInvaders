bdf749927fee88084660f19a45bbc1b43f3d4fbe
Malkrich <mathurin.lemarie@gmail.com>
2020-12-14 17:25:01 +0100

    ajout objet alien + data

---
+++ alien.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Auteur : Mathurin LEMARIE
+#Date : 14/12/2020
+#Reste à faire : déplacement verticale de l'alien,redéfinir "fen" et "can"
+
+"""
+Objet alien : 
+Atribut : 
+    - fen_pos_x : la position en abscisse de l'alien
+    - fen_pos_y : la position en ordonnée de l'alien
+Les arguments entrées pour créer l'alien sont la ligne et la colonne
+On part du principe que dans le jeux, les alien peuvent se positionner dans une grille
+de 4 ligne par 8 colonne
+la fonction move permet de déplacer l'objet sur le canvas
+"""
+
+#from tkinter import Tk,Canvas
+from data import data
+
+
+width,height,dim,dX = data()
+
+class alien:
+    def __init__(self,line,column):
+        global height,width,dim
+        #calcule des coordonnées en fonction de la hauteur et de la largeur du canvas
+        self.fen_pos_x=(width*column/8)-dim
+        self.fen_pos_y=((height/2*line)/4)-dim
+        
+        #création du rectangle que l'on enregistre dans l'attribu rect_alien
+        self.rect_alien=can.create_rectangle(self.fen_pos_x,self.fen_pos_y,self.fen_pos_x+dim,self.fen_pos_y+dim,fill='red')
+    
+    def move(self,dX):
+        global height,width,dim
+        
+        if self.fen_pos_x+dim>width:
+            dX=-dX
+        
+        if self.fen_pos_x<dim:
+            dX=-dX
+        
+        self.fen_pos_x=self.fen_pos_x+dX
+        can.coords(self.rect_alien,self.fen_pos_x,self.fen_pos_y,self.fen_pos_x+dim,self.fen_pos_y+dim)
+        fen.after(40,lambda:self.move(dX))
+
+
+"""
+#Programme de test de l'objet
+fen = Tk()
+
+width=700
+height=500
+dim=20
+dX=5
+
+can = Canvas(fen, width=width, height=height, bg='white')
+
+alien_1=alien(line=2,column=1)
+alien_1.move(dX)
+
+can.pack()
+fen.mainloop()
+"""
---
+++ data.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 14 16:35:02 2020
+
+@author: lemarie
+"""
+
+from tkinter import Tk,Canvas
+
+def data():
+    width=800
+    height=500
+    dim=20
+    dX=5
+    
+    return width,height,dim,dX
