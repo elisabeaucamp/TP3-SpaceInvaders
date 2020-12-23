118941bea73a90a5815db3e107753358c6ffe6f2
elisa <elisabeaucamp@yahoo.fr>
2020-12-23 17:00:55 +0100

    déplacement vaisseau propre + projectile

--- fond.py
+++ fond.py
@@ -1,39 +1,58 @@
 '''auteur : Elisa
 Date : 14 décembre
-To do : fond du jeu'''
+To do : création du projectile au bon endroit tant qu'on n'a pas appuyé sur espace'''
 
 from tkinter import Tk, Canvas, Button, PhotoImage, Frame, Menu
 from a_propos import about
 
-class vaisseau :
-    def __init__(self,line, column):
-        global height
-        self.MaFenetre_pos_x = width - (width - 10)
-        self.MaFenetre_pos_y = height - 10
-        self.rect_vaisseau = Canevas.create_rectangle(self.MaFenetre_pos_x, self.MaFenetre_pos_y, self.MaFenetre_pos_x + 90, self.MaFenetre_pos_y - 20, fill = 'red')
+class cVaisseau :
+    def __init__(self):
+        self.MaFenetre_pos_x = 400
+        self.MaFenetre_pos_y = 490
+        self.rect_vaisseau = Canevas.create_rectangle(self.MaFenetre_pos_x - 45, self.MaFenetre_pos_y - 10, self.MaFenetre_pos_x + 45, self.MaFenetre_pos_y + 10, fill = 'red')
 
     def deplacer (self, event):
         touche = event.keysym
-        dX =  + 20
-        dY = 10 + 20
         print(touche)
-        if touche == 'z':
-            self.MaFenetre_pos_y -= dY
-        if touche == 's':
-            self.MaFenetre_pos_y += dY
-        if touche == 'd':
-            self.MaFenetre_pos_x += dX
-        if touche == 'q':
-            self.MaFenetre_pos_x -= dX
+        #Allez à droite...
+        if touche == 'd' or touche == "Right":
+            #...si on n'est pas deja trop à droite
+            if self.MaFenetre_pos_x < width - 55 :
+                self.MaFenetre_pos_x += 10
+            #...si on l'est trop
+            else :
+                self.MaFenetre_pos_x += 0
+        #Allez à gauche...
+        if touche == 'q' or touche == "Left":
+            #...si on n'est pas déjà trop à gauche
+            if self.MaFenetre_pos_x > 55 :
+                self.MaFenetre_pos_x -= 10
+            #...si on l'est trop
+            else : 
+                self.MaFenetre_pos_x -= 0
+        Canevas.coords(self.rect_vaisseau, self.MaFenetre_pos_x - 45, self.MaFenetre_pos_y - 10, self.MaFenetre_pos_x + 40, self.MaFenetre_pos_y + 10)
 
-        Canevas.coords(self.rect_vaisseau, self.MaFenetre_pos_x - (dX/2), self.MaFenetre_pos_y - (dX/2),  self.MaFenetre_pos_x + (dX/2), self.MaFenetre_pos_y + (dX/2))
+    def getPosX(self) :
+        return int(self.MaFenetre_pos_x)
 
-    '''def projectile (self, event) :
+    def getPosY(self) :
+        return int(self.MaFenetre_pos_y)
+
+class cProjectile :
+    def __init__(self) :
+        self.MaFenetre_posx = 400
+        self.MaFenetre_posy = 480
+        self.rect_projectile = Canevas.create_rectangle(self.MaFenetre_posx - 5, self.MaFenetre_posy - 5, self.MaFenetre_posx + 5, self.MaFenetre_posy + 5, fill = 'yellow')
+    
+    def tir (self,event) :
         touche = event.keysym
-        print(touche)
-        if touche == 'space' :
-            '''
+        if touche == "space" :
             
+            self.MaFenetre_posy -= 10
+            if self.MaFenetre_posy < 0 :
+                Canevas.delete(self.rect_projectile)
+        Canevas.coords(self.rect_projectile, self.MaFenetre_posx - 5, self.MaFenetre_posy - 5, self.MaFenetre_posx + 5, self.MaFenetre_posy + 5)
+        Canevas.after(40,lambda:self.tir(event))
 
 
 #Création de la fenêtre principale
@@ -41,11 +60,9 @@ MaFenetre = Tk()
 MaFenetre.title("Space Invaders")
 width = 800
 height = 500
-dim = 20
-
 
 #Image de fond
-#photo = PhotoImage(file = 'Images/Terre.gif')
+photo = PhotoImage(file = 'Images/Terre.gif')
 
 # creation d'un widget Frame dans la fenetre principale
 Frame1 = Frame(MaFenetre,relief='groove', bg = 'yellow')
@@ -56,12 +73,14 @@ Frame2 = Frame(MaFenetre,relief='groove', bg ='pink')
 Frame2.pack(side='left',padx=10,pady=10)
 
 #Création d'un widget canvas
-Canevas = Canvas(Frame1, height = height, width = width, bg = 'black')
-#tem = Canevas.create_image(0,0, anchor = 'sw', image = photo)
-#print('Image de fond(item', item,")")
+Canevas = Canvas(Frame1, height = height, width = width)
+item = Canevas.create_image(0,0, anchor = 'sw', image = photo)
+print('Image de fond(item', item,")")
 Canevas.focus_set()
-vaisseau = vaisseau(line = height, column = 1)
-Canevas.bind('<Key>', vaisseau.deplacer)
+unVaisseau = cVaisseau()
+Canevas.bind('<Key>', unVaisseau.deplacer)
+unProjectile = cProjectile()
+Canevas.bind('<Key>', unProjectile.tir)
 Canevas.pack()
 
 #Création d'un bouton Quitter
