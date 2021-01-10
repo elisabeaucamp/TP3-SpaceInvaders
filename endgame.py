9deec3b52a0c4bc419bbfd1aa8a6f6399913d549
Malkrich <mathurin.lemarie@gmail.com>
2021-01-10 15:53:14 +0100

    ajout endgame

---
+++ endgame.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 14:14:40 2021
+
+@author: lemarie
+"""
+
+from tkinter import Canvas
+
+def end_game(window,canvas):
+    canvas.destroy()
+    
